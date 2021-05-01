ab3d1e3ee3a293988f6945b18f3d8eefac269d4b
prinxard <prinxard@gmail.com>
2021-05-01 16:42:05 +0100

    display the create-edit form

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
 useEffect(()=>{
   axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
@@ -18,19 +19,30 @@ useEffect(()=>{
 function handleSelectActivity(id: string){
   setSelectedActivity(activities.find(x => x.id === id))
 }
-function handleCancelActivity(){
+function handleCancelSelectActivity(){
   setSelectedActivity(undefined);
 }
 
+function handleFormOpen(id?: string){
+  id ? handleSelectActivity(id) : handleCancelSelectActivity();
+  setEditMode(true);
+}
+
+function handleFormClose(){
+  setEditMode(false);
+}
   return (
     <Fragment>
-     <NavBar />
+     <NavBar openForm = {handleFormOpen}/>
         <Container style={{marginTop: '7em'}}>
         <ActivityDashboard 
-        activities={activities}
-        selectedActivity={selectedActivity}
-        selectActivity={handleSelectActivity}
-        cancelSelectActivity={handleCancelActivity}
+        activities = {activities}
+        selectedActivity = {selectedActivity}
+        selectActivity = {handleSelectActivity}
+        cancelSelectActivity = {handleCancelSelectActivity}
+        editMode = {editMode}
+        openForm = {handleFormOpen}
+        closeForm = {handleFormClose}
         />
         </Container>
             
