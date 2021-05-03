0f5e8845025cc2c5dc78f1762388011df5ffafa9
prinxard <prinxard@gmail.com>
2021-05-03 12:25:01 +0100

    persisting create activity to API

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComonent';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -14,68 +14,83 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
-useEffect(()=>{
- agent.Activities.list().then(response => {
-   let activities: Activity[] = [];
+  useEffect(() => {
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = [];
 
-   response.forEach(activity => {
-     activity.date = activity.date.split('T')[0];
-     activities.push(activity);
-   })
+      response.forEach(activity => {
+        activity.date = activity.date.split('T')[0];
+        activities.push(activity);
+      })
 
-    setActivities(activities);
-    setLoading(false);
-  })
-}, [])
+      setActivities(activities);
+      setLoading(false);
+    })
+  }, [])
 
-function handleSelectActivity(id: string){
-  setSelectedActivity(activities.find(x => x.id === id))
-}
-function handleCancelSelectActivity(){
-  setSelectedActivity(undefined);
-}
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
 
-function handleFormOpen(id?: string){
-  id ? handleSelectActivity(id) : handleCancelSelectActivity();
-  setEditMode(true);
-}
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
 
-function handleFormClose(){
-  setEditMode(false);
-}
+  function handleFormClose() {
+    setEditMode(false);
+  }
 
-function handleCreateOrEditActivity(activity:Activity) {
-  setSubmitting(true);
-  activity.id ? setActivities
-  ([...activities.filter(x => x.id !== activity.id), activity])
-  : setActivities([...activities, {...activity, id: uuid() }]);
-  setEditMode(false);
-  setSelectedActivity(activity);
-}
+  function handleCreateOrEditActivity(activity: Activity) {
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setSubmitting(true)
+        if (activity.id) {
+          setActivities([...activities.filter(x => x.id !== activity.id), activity])
+          setSelectedActivity(activity);
+          setEditMode(false);
+          setSubmitting(false);
+        }
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]) 
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
+  }
 
-function handleDeleteActivity(id: string) {
-  setActivities([...activities.filter(x => x.id !== id)])
-}
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
 
-if (loading) return <LoadingComponent content = 'Loading app' />
+  if (loading) return <LoadingComponent content='Loading app' />
 
   return (
     <Fragment>
-     <NavBar openForm = {handleFormOpen}/>
-        <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard 
-        activities = {activities}
-        selectedActivity = {selectedActivity}
-        selectActivity = {handleSelectActivity}
-        cancelSelectActivity = {handleCancelSelectActivity}
-        editMode = {editMode}
-        openForm = {handleFormOpen}
-        closeForm = {handleFormClose}
-        createOrEdit = {handleCreateOrEditActivity}
-        deleteActivity = {handleDeleteActivity}
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities = {activities}
+          selectedActivity = {selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+          submitting = {submitting}
         />
-        </Container>
-            
+      </Container>
+
     </Fragment>
   );
 }
