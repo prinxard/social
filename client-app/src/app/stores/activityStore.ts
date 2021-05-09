191d7a8c027395b92a052610c22176c731b6b397
prinxard <prinxard@gmail.com>
2021-05-09 21:05:29 +0100

    fixed the strict mode bug in the console

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,17 +14,25 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
-        this.loadingInitial = true;
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
-            activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                this.activities.push(activity);
-            })
-            this.loadingInitial = false;
+            
+
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activities.push(activity);
+                })
+                this.setLoadingInitial(false);              
         }
         catch (error) {
-            this.loadingInitial = false;
+             
+            this.setLoadingInitial(false);
+             
+            
         }
+    }
+    setLoadingInitial = (state: boolean)  => {
+        this.loadingInitial = state;
     }
 }
