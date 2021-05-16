145e38b204b02d569e4574bf2e62eff973ca62ec
prinxard <prinxard@gmail.com>
2021-05-16 14:58:02 +0100

    Create an activity using mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComonent';
-import { v4 as uuid } from 'uuid';
+
 import { useStore } from '../stores/store.ts';
 import { observer } from 'mobx-react-lite';
 
@@ -14,8 +14,6 @@ function App() {
   const {activityStore} = useStore();
 
   const [activities, setActivities] = useState<Activity[]>([])
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
@@ -24,28 +22,7 @@ function App() {
 
   
 
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setSubmitting(true)
-        if (activity.id) {
-          setActivities([...activities.filter(x => x.id !== activity.id), activity])
-          setSelectedActivity(activity);
-          setEditMode(false);
-          setSubmitting(false);
-        }
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]) 
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
+  
 
   function handleDeleteActivity(id: string) {
     setSubmitting(true);
@@ -65,7 +42,6 @@ function App() {
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
           activities = {activityStore.activities}
-          createOrEdit={handleCreateOrEditActivity}
           deleteActivity={handleDeleteActivity}
           submitting = {submitting}
         />
