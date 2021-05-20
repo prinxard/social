fca25d936a43d8800e361d75e23ebadf705609b9
prinxard <prinxard@gmail.com>
2021-05-20 11:13:24 +0100

    Added react router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,23 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 import { Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
-import LoadingComponent from './LoadingComonent';
-
 import { useStore } from '../stores/store.ts';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
 
-  const {activityStore} = useStore();
-
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
+ 
   return (
     <Fragment>
       <NavBar  />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+       <Route exact path='/' component={HomePage}/>
+       <Route path='/activities' component={ActivityDashboard}/>
+       <Route path='/createActivity' component={ActivityForm}/>
       </Container>
 
     </Fragment>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, List } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComonent';
 import { useStore } from '../../../app/stores/store.ts';
 import ActivityDetails from '../details/Activitydetails';
 import ActivityForm from '../form/ActivityForm';
@@ -10,6 +11,13 @@ import ActivityList from './ActivityList';
 export default observer(function ActivityDashboard() {
         const {activityStore} = useStore();
         const {selectedActivity, editMode} = activityStore;
+
+        useEffect(() => {
+          activityStore.loadActivities();
+        }, [activityStore])
+      
+        if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+      
     return (
         <Grid>
             <Grid.Column width='10'>
