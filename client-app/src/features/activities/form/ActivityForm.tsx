e0d9e314095f3333ff5cdb434ea136961eb17495
prinxard <prinxard@gmail.com>
2021-07-03 18:07:05 +0100

    grouped activities by date

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -4,7 +4,7 @@ import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store.ts';
 import {v4 as uuid} from 'uuid';
-import { useHistory, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComonent';
 
@@ -62,7 +62,7 @@ export default observer (function ActivityForm() {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button loading = {loading} floated='right' positive type='submit' content='Submit' />
-                <Button floated='right' type='submit' content='Cancel' />
+                <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
             </Form>
         </Segment>
     )
