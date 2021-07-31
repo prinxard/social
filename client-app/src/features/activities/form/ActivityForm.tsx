d84866feb2d598dcf6e071432ec94ea10f3d42a9
prinxard <prinxard@gmail.com>
2021-07-31 22:51:21 +0100

    added formik to handle forms

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,18 +3,20 @@ import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store.ts';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComonent';
+import { Formik } from 'formik';
+import { values } from 'mobx';
 
 
 
-export default observer (function ActivityForm() {
+export default observer(function ActivityForm() {
     const history = useHistory();
-    const {activityStore} = useStore();
-    const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
-    const {id} = useParams<{id: string}>();
+    const { activityStore } = useStore();
+    const { createActivity, updateActivity, loading, loadActivity, loadingInitial } = activityStore;
+    const { id } = useParams<{ id: string }>();
 
     const [activity, setActivity] = useState({
         id: '',
@@ -26,44 +28,49 @@ export default observer (function ActivityForm() {
         venue: ''
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
-    }, [id, loadActivity]) ;
-    
+    }, [id, loadActivity]);
 
-     function handleSubmit() {
-      if (activity.id.length === 0){
-          let newActivity = {
-              ...activity,
-              id: uuid()
-          };
-          createActivity(newActivity).then(()=> history.push(`/activities/${newActivity.id}`))
-      }
-      else{
-          updateActivity(activity).then(()=> history.push(`/activities/${activity.id}`))
-      }
-     }
 
-     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-         const{name, value} = event.target;
-         setActivity({...activity, [name]: value});
+    //  function handleSubmit() {
+    //   if (activity.id.length === 0){
+    //       let newActivity = {
+    //           ...activity,
+    //           id: uuid()
+    //       };
+    //       createActivity(newActivity).then(()=> history.push(`/activities/${newActivity.id}`))
+    //   }
+    //   else{
+    //       updateActivity(activity).then(()=> history.push(`/activities/${activity.id}`))
+    //   }
+    //  }
 
-     }
-     
+    //  function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    //      const{name, value} = event.target;
+    //      setActivity({...activity, [name]: value});
+
+    //  }
+
     //  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
-            <Form onSubmit={handleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
-                <Form.TextArea placeholder='Description'value={activity.description} name='description' onChange={handleInputChange} />
-                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
-                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
-                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
-                <Button loading = {loading} floated='right' positive type='submit' content='Submit' />
-                <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
-            </Form>
+            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+                {({ values: activity, handleChange, handleSubmit }) => (
+                    <Form onSubmit={handleSubmit} autoComplete='off'>
+                        <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleChange} />
+                        <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleChange} />
+                        <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleChange} />
+                        <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleChange} />
+                        <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleChange} />
+                        <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleChange} />
+                        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+                        <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
+                    </Form>
+                )}
+            </Formik>
+    
         </Segment>
     )
 })
