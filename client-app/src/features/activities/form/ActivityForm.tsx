b0b2be1c0186193c43194957271c8bbc92932627
prinxard <prinxard@gmail.com>
2021-08-01 08:03:55 +0100

    added reusable component for text input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,16 @@
 import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, useState } from 'react';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store.ts';
 import { v4 as uuid } from 'uuid';
 import { Link, useHistory, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComonent';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import { values } from 'mobx';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
 
 
 
@@ -27,7 +29,14 @@ export default observer(function ActivityForm() {
         city: '',
         venue: ''
     })
-
+    const validatonSchema = Yup.object({
+        title: Yup.string().required('The activity title is required'),
+        description: Yup.string().required('The activity description is required'),
+        category: Yup.string().required(),
+        date: Yup.string().required(),
+        venue: Yup.string().required(),
+        city: Yup.string().required(),
+    })
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
     }, [id, loadActivity]);
@@ -52,21 +61,25 @@ export default observer(function ActivityForm() {
 
     //  }
 
-    //  if (loadingInitial) return <LoadingComponent content='Loading activity...' />
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
-                {({  handleSubmit }) => (
+            <Formik
+                validationSchema={validatonSchema}
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
+                {({ handleSubmit }) => (
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
-                        <Field placeholder='Title'  name='title'  />
-                        <Field placeholder='Description'  name='description'  />
-                        <Field placeholder='Category'  name='category'  />
-                        <Field type='date' placeholder='Date'  name='date'  />
-                        <Field placeholder='City'  name='city'  />
-                        <Field placeholder='Venue'  name='venue'  />
-                        <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                        <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
+                            <MyTextInput name='title' placeholder='title' />
+                            <MyTextInput placeholder='Description' name='description' />
+                            <MyTextInput placeholder='Category' name='category' />
+                            <MyTextInput placeholder='Date' name='date' />
+                            <MyTextInput placeholder='City' name='city' />
+                            <MyTextInput placeholder='Venue' name='venue' />
+                            <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+                            <Button as={Link} to='/activities' floated='right' type='submit' content='Cancel' />
                     </Form>
                 )}
             </Formik>
