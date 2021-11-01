352d369c32c1c919c74e96829da847d8efaa3530
prinxard <prinxard@gmail.com>
2021-11-01 17:23:27 +0100

    created auth policy

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
