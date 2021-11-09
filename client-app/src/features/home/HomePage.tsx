f7e92f48419024eafcabfaf6ae58a3eed71ef760
prinxard <prinxard@gmail.com>
2021-11-09 13:10:09 +0100

    display username on navbar on login

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Header, Segment,Image, Button } from 'semantic-ui-react';
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
-export default function HomePage(){
-    return(
-      <Segment inverted textAlign='center' className='masthead'> 
-        <Container text>
-            <Header as='h1' inverted>
-                <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
-                Reactivities
-            </Header>
-            <Header as='h2' inverted content='Welcome to reactivities' />
-            <Button as={Link} to='/login' size='huge' inverted>
-                Login!
+export default observer(function HomePage() {
+  const { userStore } = useStore();
+  return (
+    <Segment inverted textAlign="center" className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as="h2" inverted content="Welcome to reactivities" />
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go to activities!
             </Button>
-        </Container>
-      </Segment>
-    )
-}
+          </>
+        ) : (
+          <Button as={Link} to="/login" size="huge" inverted>
+            Login!
+          </Button>
+        )}
+      </Container>
+    </Segment>
+  );
+});
